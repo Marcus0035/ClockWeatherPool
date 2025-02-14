e15494cfd0851341cfff282de5c7337d3b770420
Marek Dvořáček <marek.dvoracek606@seznam.cz>
2025-02-14 17:24:17 +0100

    CSSstyling

---
+++ ClockWeatherPool/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockWeatherPool
+{
+    public class Settings
+    {
+        public string WeatherApiKey { get; set; } = string.Empty;
+    } 
+}
